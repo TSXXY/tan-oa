4f964d2fe394dbd623443b46673a4bbc78372f82
Tan <ts20010411@163.com>
2023-04-12 17:54:26 +0800

    完成全局异常处理

---
+++ common/service-util/src/main/java/com/tan/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.tan.exception;
+
+import com.tan.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.error("服务器出现了未知错误");
+    }
+}
